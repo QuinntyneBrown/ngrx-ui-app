415ac4d17c7b4c96176d2b81adf7e9a9d697374b
Quinntyne Brown <quinntynebrown@gmail.com>
2016-07-03 22:14:45 -0400

    initial

---
+++ config/webpack.dev.js
@@ -0,0 +1,35 @@
+﻿module.exports = {
+    entry: {
+        'polyfills': './src/polyfills.ts',
+        'vendor': './src/vendor.ts',
+        'app': './src/main.ts'
+    },
+
+    output: {
+        path: "/dist",
+        publicPath: 'http://localhost:8080/',
+        filename: '[name].js',
+        chunkFilename: '[id].chunk.js'
+    },
+
+    resolve: {
+        extensions: ['', '.js', '.ts']
+    },
+
+    module: {
+        loaders: [
+          {
+              test: /\.ts$/,
+              loader: 'ts'
+          },
+          {
+              test: /\.html$/,
+              loader: 'html'
+          },
+        ]
+    },
+    resolve: {
+        extensions: ['', '.webpack.js', '.web,js', '.ts', '.js']
+    }
+
+}
---
+++ src/main.ts
@@ -0,0 +1,4 @@
+﻿import { bootstrap }  from "@angular/platform-browser-dynamic";
+import { AppComponent } from "./app/app.component.ts";
+
+bootstrap(AppComponent, []).catch(err => console.log(err));
